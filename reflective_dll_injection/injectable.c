0a1543a1c7e657bcf26805bbb16821b3bef69da5
alexP-bu <56131375+alexP-bu@users.noreply.github.com>
2022-11-29 13:12:53 -0500

    copied injectable

---
+++ reflective_dll_injection/injectable.c
@@ -0,0 +1,16 @@
+#include <windows.h>
+
+//compile with command: gcc -shared -o injectable.dll injectable.c
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved){
+    switch(fdwReason){
+        case DLL_PROCESS_ATTACH: {
+            MessageBoxA(NULL, "hello world, from inside your process!", "dll injection done", 0x00000000L);
+        }
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+    return TRUE;
+}
