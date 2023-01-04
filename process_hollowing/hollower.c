69c04ef516d7ec360688b1ab1e833b9eca7a8b1e
alexP-bu <apozin@bu.edu>
2023-01-03 20:45:45 -0500

    setup process in suspended state

--- process_hollowing/hollower.c
+++ process_hollowing/hollower.c
@@ -1,6 +1,38 @@
-//idea from
-//https://github.com/m0n0ph1/Process-Hollowing
+//idea from https://github.com/m0n0ph1/Process-Hollowing
+#include <windows.h>
 
 int main(int argc, char* argv[]){
+    //first create a process in suspended state
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    if(!CreateProcessA(NULL, 
+                       NULL, 
+                       NULL, 
+                       NULL, 
+                       FALSE, 
+                       CREATE_SUSPENDED, 
+                       NULL, 
+                       NULL, 
+                       &si, 
+                       &si)){
+        printf("[!] ERROR CREATING PROCESS");
+        //next, find the base address from the PEB block
+
+        //then, read the NT headers
+
+        //after this, unmap destination section with NtUnmapViewOfSection
+
+        //allocate memory for new image with virtuallocex, 
+        //the image base stored must be set to the destination image base address
+        //the optional header needs to be patched
+
+        //copy the image with writeprocessmemory
+        
+        //rebase the source image with the .reloc section, IMAGE_DATA_DIRECTORY (IMAGE_DIRECTORY_ENTRY_BASERELOC)
+
+        //with the source image loaded into the target process, update EAX register with the thread context
+
+        //resume the thread
+    }
     return 0;
 }
