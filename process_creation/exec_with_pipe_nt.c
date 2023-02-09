d5daaef75eee6e2e6c59f6ec240c4c01e61fc412
alexP-bu <apozin@bu.edu>
2023-02-09 02:23:40 -0500

    IT WORKS WTF

--- process_creation/exec_with_pipe_nt.c
+++ process_creation/exec_with_pipe_nt.c
@@ -1,6 +1,23 @@
 #include "printfile.h"
 
 #define BUFSIZE 4096
+#define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
+
+typedef NTSTATUS (NTAPI* ntAllocateVirtualMemory)(
+  HANDLE ProcessHandle,
+  PVOID *BaseAddress,
+  ULONG_PTR ZeroBits,
+  PSIZE_T RegionSize,
+  ULONG AllocationType,
+  ULONG Protect
+);
+
+typedef NTSTATUS (NTAPI* ntFreeVirtualMemory)(
+  HANDLE ProcessHandle,
+  PVOID *BaseAddress,
+  PSIZE_T RegionSize,
+  ULONG FreeType
+);
 
 BOOL readFromPipe(HANDLE hReadPipe, PBYTE lpBuffer){
   DWORD lpTotalBytesAvail = 0;
@@ -35,33 +52,50 @@ BOOL readFromPipe(HANDLE hReadPipe, PBYTE lpBuffer){
 }
 
 int main(int argc, char** argv){
-  DWORD dwArgsLen = 0;
-  for(DWORD i = 1; i < argc; i++){
+  
+  //lets get ntdll and functions we need from it
+  HANDLE hProcess = NULL;
+  hProcess = GetCurrentProcess();
+  if(!hProcess){
+    printf("[!] Error getting current process: %d\n", GetLastError());
+    return -1;
+  }
+  HMODULE hNtdll = NULL;
+  hNtdll = LoadLibraryA("ntdll");
+  if(!hNtdll){
+    printf("[!] Error loading ntdll: %d\n", GetLastError());
+    return -1;
+  }
+  FARPROC fpNtAllocateVirtualMemory = GetProcAddress(hNtdll, "NtAllocateVirtualMemory");
+  FARPROC fpNtFreeVirtualMemory = GetProcAddress(hNtdll, "NtFreeVirtualMemory");
+  ntAllocateVirtualMemory NtAllocateVirtualMemory = (ntAllocateVirtualMemory)fpNtAllocateVirtualMemory;
+  ntFreeVirtualMemory NtFreeVirtualMemory = (ntFreeVirtualMemory)fpNtFreeVirtualMemory;
+
+  //get length of command line args
+  SIZE_T dwArgsLen = 0;
+  for(SIZE_T i = 1; i < argc; i++){
     dwArgsLen += 1; //spaces
     dwArgsLen += strlen(argv[i]);
   }
+
   //let's remove our use of malloc by using HeapCreate, HeapAlloc, HeapFree, HeapDestroy
-  HANDLE hHeap;
-  hHeap = HeapCreate(
-    HEAP_CREATE_ENABLE_EXECUTE,
+  //finally let's bypass HeapAlloc with a direct call to NtAllocateVirtualMemory
+  NTSTATUS ntStatus;
+  SIZE_T tCommandLine = (sizeof(BYTE) * (strlen("cmd /c "))) + (sizeof(BYTE) * (dwArgsLen + 1));
+  PBYTE lpCommandLine = 0;
+  ntStatus = NtAllocateVirtualMemory(
+    hProcess,
+    (PVOID)&lpCommandLine,
     0,
-    0
+    &tCommandLine,
+    MEM_COMMIT | MEM_RESERVE,
+    PAGE_EXECUTE_READWRITE
   );
-  if(hHeap == INVALID_HANDLE_VALUE){
-    printf("Error creating heap%d\n", GetLastError());
+  if(!NT_SUCCESS(ntStatus)){
+    printf("[!] Error allocating virtual memory for command line: %x\n", ntStatus);
     return -1;
-  }
-  SIZE_T tSize = (sizeof(BYTE) * (strlen("cmd /c "))) + (sizeof(BYTE) * (dwArgsLen + 1));
-  PBYTE lpCommandLine = NULL;
-  lpCommandLine = HeapAlloc(
-    hHeap,
-    HEAP_ZERO_MEMORY,
-    tSize
-  );
-  if(!lpCommandLine){
-    printf("[!] Error allocating memory for command line!");
-    return -1;
-  }
+  } 
+
   //format: cmd /c program arg0 arg1 
   sprintf(lpCommandLine, "cmd /c ");
   for(DWORD i = 1; i < argc; i++){
@@ -69,11 +103,14 @@ int main(int argc, char** argv){
   }
   sprintf(lpCommandLine + strlen(lpCommandLine), "%c", '\0');
   printf("got command line: %s\nlen: %d\n", lpCommandLine, strlen(lpCommandLine)); //DEBUG
+
   //create pipe
   HANDLE hReadPipe;
   HANDLE hWritePipe;
   SECURITY_ATTRIBUTES sa;
-  ZeroMemory(&sa, sizeof(sa));
+  RtlZeroMemory(&sa, sizeof(sa));
+
+  //createpipe calls NtOpenFile
   if(!CreatePipe(
     &hReadPipe,
     &hWritePipe,
@@ -83,7 +120,9 @@ int main(int argc, char** argv){
     printf("[!] Error creating pipe: %d\n", GetLastError());
     return -1;
   };
+
   //make sure only write end is inherited
+  //instead of using sethandleinformation, we can use ntqueryobject + ntsetinformationobject
   if(!SetHandleInformation(
     hWritePipe, 
     HANDLE_FLAG_INHERIT, 
@@ -92,14 +131,16 @@ int main(int argc, char** argv){
     printf("[!] Error setting handle information: %d\n", GetLastError());
     return -1;
   };
+
+  //create process
   STARTUPINFO si;
-  ZeroMemory(&si, sizeof(si));
+  RtlZeroMemory(&si, sizeof(si));
   si.cb = sizeof(si);
   si.hStdOutput = hWritePipe;
   si.hStdError = hWritePipe;
   si.dwFlags = STARTF_USESTDHANDLES;
   PROCESS_INFORMATION pi;
-  ZeroMemory(&pi, sizeof(pi));
+  RtlZeroMemory(&pi, sizeof(pi));
   if(!CreateProcessA(
     NULL,
     lpCommandLine,
@@ -115,15 +156,20 @@ int main(int argc, char** argv){
     printf("[!] Error creating process: %d\n", GetLastError());
     return -1;
   }
+
   //read from pipe
   PBYTE lpBuffer = NULL;
-  lpBuffer = HeapAlloc(
-    hHeap,
-    HEAP_ZERO_MEMORY,
-    sizeof(BYTE) * BUFSIZE
+  SIZE_T lpBufferSize = (SIZE_T)(sizeof(BYTE) * BUFSIZE);
+  ntStatus = NtAllocateVirtualMemory(
+    hProcess,
+    (PVOID)&lpBuffer,
+    0,
+    &lpBufferSize,
+    MEM_COMMIT | MEM_RESERVE,
+    PAGE_EXECUTE_READWRITE
   );
-  if(!lpBuffer){
-    printf("[!] Error allocating read buffer on heap: %d\n", GetLastError());
+  if(!NT_SUCCESS(ntStatus)){
+    printf("[!] Error allocating virtual memory for output buffer: %x", ntStatus);
     return -1;
   }
   while(WaitForSingleObject(pi.hProcess, 50)){
@@ -140,8 +186,25 @@ int main(int argc, char** argv){
   CloseHandle(pi.hProcess);
   CloseHandle(hWritePipe);
   CloseHandle(hReadPipe);
-  HeapFree(hHeap, 0, lpCommandLine);
-  HeapFree(hHeap, 0, lpBuffer);
-  HeapDestroy(hHeap);
+  ntStatus = NtFreeVirtualMemory(
+    hProcess,
+    (PVOID)&lpCommandLine,
+    0,
+    MEM_RELEASE
+  );
+  if(!NT_SUCCESS(ntStatus)){
+    printf("[!] Error freeing commandline memory, %x\n", ntStatus);
+    return -1;
+  }
+  ntStatus = NtFreeVirtualMemory(
+    hProcess,
+    (PVOID)&lpBuffer,
+    0,
+    MEM_RELEASE
+  );
+  if(!NT_SUCCESS(ntStatus)){
+    printf("[!] Error freeing output buffer memory, %x\n", ntStatus);
+    return -1;
+  }
   return 0;
 }
